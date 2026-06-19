e443e694ff597562acc01c01b99d9df38dbedd54
bawercoskun47 <92177093+bawercoskun47@users.noreply.github.com>
2026-06-20 00:26:58 +0300

    chore(deps): add audioplayers, shimmer, lottie, share_plus, in_app_review, connectivity_plus

--- zankurd_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ zankurd_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,17 +6,25 @@ import FlutterMacOS
 import Foundation
 
 import app_links
+import audioplayers_darwin
+import connectivity_plus
 import firebase_core
 import firebase_crashlytics
+import in_app_review
 import path_provider_foundation
+import share_plus
 import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
+  InAppReviewPlugin.register(with: registry.registrar(forPlugin: "InAppReviewPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
